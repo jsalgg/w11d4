dbc8fe2a2968595b232a7a58b8fe9bbb6cb05e77
Andrew Loeffler <aloeffler@gmail.com>
2021-04-22 14:26:18 -0400

    got async handler finished

--- routes.js
+++ routes.js
@@ -1,12 +1,25 @@
 // ./routes.js
 
 const express = require("express");
+const db = require('./db/models')
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+const asyncHandler = (handler) => {
+  return (req, res, next) => {
+    return handler(req, res, next).catch(next)
+  }
+};
+
+router.get("/", asyncHandler (async(req, res) => {
   // throw new Error('This is a test error!');
-  res.render("index", { title: "Home" });
-});
+  const books = await db.Book.findAll(
+    { order: [['title', 'ASC']]}
+  );
+
+  res.render('book-list', { title: 'Books', books });
+
+
+}));
 
 module.exports = router;
